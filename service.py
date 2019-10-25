7eda21e4ea4566fc32f6238cd1a5e5c8b0c7a3da
Jason Barrie Morley <jason.morley@inseven.co.uk>
2019-10-25 10:47:42 -0700

    Introduce a new API end-point and return JSON.

--- service.py
+++ service.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
 
 import offset
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
+def index():
+	return ""
+
+
+@app.route('/api/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return jsonify({'offset': offset.offset("America/Los_Angeles")})
 
 
 if __name__ == '__main__':
