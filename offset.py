55b290aadeed2760cd40beeac44db90fc7937959
Jason Barrie Morley <jason.morley@inseven.co.uk>
2019-10-25 12:59:40 -0700

    Remove unused command-line functionality.

--- offset.py
+++ offset.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
 import datetime
 import pytz
 
@@ -9,14 +8,3 @@ def offset(timezone):
     tz = pytz.timezone(timezone)
     offset = tz.utcoffset(datetime.datetime.now()).total_seconds()
     return offset
-
-
-def main():
-    parser = argparse.ArgumentParser(description="Lightweight tool to give the UTC offset for a given timezone")
-    parser.add_argument("timezone")
-    options = parser.parse_args()
-    print(offset(options.timezone))
-
-
-if __name__ == "__main__":
-	main()
